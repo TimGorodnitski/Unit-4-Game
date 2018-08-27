385ca5612d64aea2919f4f22ee74524638c2ca31
TimG <timgm3@gmail.com>
2018-08-27 16:12:26 -0700

    Attack button works, just need win and lose conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,51 @@
-// initial health values
+// initial stat values
 let rickHealth = 110;
-let mortyHealth = 90;
-let bethHealth = 80;
-let jerryHealth = 75;
-
 let rickAttack = 10;
-let rickCounter = 5;
+let rickDefense = 15;
+
+let mortyHealth = 90;
 let mortyAttack = 10;
-let mortyCounter = 5;
+let mortyDefense = 15;
+
+let bethHealth = 80;
 let bethAttack = 10;
-let bethCounter = 5;
+let bethDefense = 10;
+
+let jerryHealth = 75;
 let jerryAttack = 10;
-let jerryCounter = 5;
+let jerryDefense = 5;
 
 let myBaseAttack = 0;
 let myCharAttack = 0;
 let myCharHealth = 0;
+let attackCount = 1;
+
 
 let opponentHealth = 0;
-let opponentCounter = 0;
+let opponentDefense = 0;
 
 
 
 
-// sets health text to initial health
-// $("#rickHealth").text(rickHealth);
-// $("#mortyHealth").text(mortyHealth);
-// $("#bethHealth").text(bethHealth);
-// $("#jerryHealth").text(jerryHealth);
+// // sets text to initial values
+$("#rickHealth").text(rickHealth);
+$("#mortyHealth").text(mortyHealth);
+$("#bethHealth").text(bethHealth);
+$("#jerryHealth").text(jerryHealth);
+
+$("#rickAttack").text(rickAttack);
+$("#mortyAttack").text(mortyAttack);
+$("#bethAttack").text(bethAttack);
+$("#jerryAttack").text(jerryAttack);
+
+$("#rickDefense").text(rickDefense);
+$("#mortyDefense").text(mortyDefense);
+$("#bethDefense").text(bethDefense);
+$("#jerryDefense").text(jerryDefense);
+
+
+
+
 
 // sets phase to character select
 let stage = 0;
@@ -41,6 +59,9 @@ $(".character").on("click", function () {
     if (stage === 0) {
 
         myCharHealth = $(this).find("span.charHealth").text();
+        myBaseAttack = $(this).find("span.charAttack").text();
+        myCharAttack = $(this).find("span.charAttack").text();
+        $(this).addClass("myChar");
         $(".stage1").show();
         $(".stage2").hide();
         $("#enemiesRemaining").append($(".character"));
@@ -52,7 +73,8 @@ $(".character").on("click", function () {
 
 
         opponentHealth = $(this).find('span.charHealth').text();
-        opponentCounter = $(this).find('span.charDefense').text();
+        opponentDefense = $(this).find('span.charDefense').text();
+        $(this).addClass("myOpp");
         $("#defenderDiv").append(this);
         $(".stage2").show();
         $(".stage3").hide();
@@ -63,7 +85,19 @@ $(".character").on("click", function () {
 
 $("#attackButton").on("click", function () {
 
-
+    opponentHealth -= myCharAttack;
+    $(".myOpp").find('span.charHealth').text(opponentHealth);
+    if (opponentHealth <= 0) {
+        $(".myOpp").remove();
+        stage = 1;
+        $(".stage2").hide();
+        $(".stage4").show();
+    };
+    myCharHealth -= opponentDefense;
+    $(".myChar").find('span.charHealth').text(myCharHealth);
+    attackCount++;
+    myCharAttack = myBaseAttack * attackCount;
+    $(".myChar").find('span.charAttack').text(myCharAttack);
 
 
 });
